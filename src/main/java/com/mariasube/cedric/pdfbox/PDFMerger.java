4925a6d4b15e05ae901b3fb8c39756755edbf645
Anonymous <anonymous@noemail.com>
2015-10-09 15:09:23 +0000

    Adds page numbers to PDF file.

--- src/main/java/com/mariasube/cedric/pdfbox/PDFMerger.java
+++ src/main/java/com/mariasube/cedric/pdfbox/PDFMerger.java
@@ -1,10 +1,17 @@
 package com.mariasube.cedric.pdfbox;
 
 import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.util.PDFMergerUtility;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class PDFMerger {
 
@@ -32,6 +39,75 @@ public class PDFMerger {
         String destinationFilePath = "./pdf/MergedFile.pdf";
         File mergedFile = pdfMerger.mergeDocuments(sourcePdf1, sourcePdf2, destinationFilePath);
         System.out.println("Merged file size :  " + mergedFile.length() + ".");
+
+        File numberedAndMergedFile = pdfMerger.addPageNumbers(mergedFile, "./pdf/NumberedFile.pdf");
+        System.out.println("Numbered file size :  " + numberedAndMergedFile.length() + ".");
+    }
+
+    public File addPageNumbers(File file, String newFilePath) {
+        File numberedFile = new File(newFilePath);
+        try {
+            numberedFile.getParentFile().mkdir();
+            numberedFile.createNewFile();
+        } catch (IOException ioe) {
+            System.out.println("Unable to create " + numberedFile.getAbsolutePath());
+            ioe.printStackTrace();
+        }
+        PDDocument doc = null;
+        try {
+            doc = PDDocument.load(file);
+
+            List<PDPage> allPages = doc.getDocumentCatalog().getAllPages();
+            PDFont font = PDType1Font.HELVETICA_BOLD;
+            float fontSize = 36.0f;
+
+            for (int i = 0; i < allPages.size(); i++) {
+                PDPage page = (PDPage)allPages.get(i);
+                PDRectangle pageSize = page.findMediaBox();
+                String message = Integer.toString(i + 1);
+                float stringWidth = font.getStringWidth(message) * fontSize / 1000f;
+                // calculate to center of the page
+                int rotation = page.findRotation();
+                boolean rotate = rotation == 90 || rotation == 270;
+                float pageWidth = rotate ? pageSize.getHeight() : pageSize.getWidth();
+                float pageHeight = rotate ? pageSize.getWidth() : pageSize.getHeight();
+                double centeredXPosition = rotate ? pageHeight / 2f : (pageWidth - stringWidth) / 2f;
+                double centeredYPosition = rotate ? (pageWidth - stringWidth) / 2f : pageHeight / 2f;
+                // append the content to the existing stream
+                PDPageContentStream contentStream = new PDPageContentStream(doc, page, true, true, true);
+                contentStream.beginText();
+                // set font and font size
+                contentStream.setFont(font, fontSize);
+                // set text color to red
+                contentStream.setNonStrokingColor(255, 0, 0);
+                if (rotate) {
+                    // rotate the text according to the page rotation
+                    contentStream.setTextRotation(Math.PI / 2, centeredXPosition, centeredYPosition);
+                } else {
+                    contentStream.setTextTranslation(centeredXPosition, centeredYPosition);
+                }
+                contentStream.drawString(message);
+                contentStream.endText();
+                contentStream.close();
+            }
+
+            try {
+                doc.save(numberedFile);
+            } catch (COSVisitorException cosve) {
+                cosve.printStackTrace();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            if (doc != null) {
+                try {
+                    doc.close();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+        }
+        return numberedFile;
     }
 
     public File mergeDocuments(String sourcePdf1, String sourcePdf2, String destinationFilePath) {
